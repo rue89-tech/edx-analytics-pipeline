07e9f7e81071439a9374dede817bee1410264c94
Gabe Mulley <gabe@edx.org>
2015-06-08 08:57:38 -0400

    modified paypal stuff

---
+++ edx/analytics/tasks/reports/paypal.py
@@ -0,0 +1,132 @@
+import datetime
+import logging
+import luigi
+import paypalrestsdk
+import json
+
+from edx.analytics.tasks.url import get_target_from_url
+from edx.analytics.tasks.url import url_path_join
+from edx.analytics.tasks.util.hive import HivePartition
+from edx.analytics.tasks.util.overwrite import OverwriteOutputMixin
+
+# Tell urllib3 to switch the ssl backend to PyOpenSSL.
+# see https://urllib3.readthedocs.org/en/latest/security.html#pyopenssl
+import urllib3.contrib.pyopenssl
+urllib3.contrib.pyopenssl.inject_into_urllib3()
+
+log = logging.getLogger(__name__)
+
+
+class PullFromPaypalTaskMixin(OverwriteOutputMixin):
+
+    client_mode = luigi.Parameter(
+        default_from_config={'section': 'paypal', 'name': 'client_mode'}
+    )
+    client_id = luigi.Parameter(
+        default_from_config={'section': 'paypal', 'name': 'client_id'},
+        significant=False,
+    )
+    # Making this 'insignificant' means it won't be echoed in log files.
+    client_secret = luigi.Parameter(
+        default_from_config={'section': 'paypal', 'name': 'client_secret'},
+        significant=False,
+    )
+
+
+class SinglePullFromPaypalTask(PullFromPaypalTaskMixin, luigi.Task):
+    """
+    A task that reads out of a remote Paypal account and writes to a file in raw JSON lines format.
+
+    A complication is that this needs to be performed with more than one account.
+
+    Inputs also include the interval over which to request daily dumps.
+
+    Output should be incremental.  That is, this task can be run periodically with
+    contiguous time intervals requested, and the output should properly accumulate.
+    """
+
+    output_root = luigi.Parameter()
+    run_date = luigi.DateParameter(default=datetime.datetime.utcnow().date())
+
+    def initialize(self):
+        log.debug('Initializing paypalrestsdk')
+        paypalrestsdk.configure({
+            'mode': self.client_mode,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret
+        })
+
+    def run(self):
+        self.initialize()
+        self.remove_output_on_overwrite()
+        all_payments = []
+
+        end_date = self.run_date + datetime.timedelta(days=1)
+        request_args = {
+            'start_time': "{}T00:00:00Z".format(self.run_date.isoformat()),  # pylint: disable=no-member
+            'end_time': "{}T00:00:00Z".format(end_date.isoformat()),
+            'count': 20
+        }
+
+        log.debug('Requesting payments')
+
+        # Paypal REST payments API: https://developer.paypal.com/docs/integration/direct/rest-payments-overview/
+        # API Reference: https://developer.paypal.com/docs/api/
+        payment_history = paypalrestsdk.Payment.all(request_args)
+        with self.output().open('w') as output_file:
+            if payment_history.payments is None:
+                log.error('No payments found')
+            else:
+                self.write_payments_from_response(payment_history, output_file)
+
+            while payment_history.next_id is not None:
+                request_args['start_id'] = payment_history.next_id
+                payment_history = paypalrestsdk.Payment.all(request_args)
+                self.write_payments_from_response(payment_history, output_file)
+
+    def write_payments_from_response(self, payment_history, output_file):
+        log.debug('Received %d payments from paypal', payment_history.count)
+        if payment_history.count != len(payment_history.payments):
+            log.error('Invalid response, payments do not match count: count=%d, len(payments)=%d', payment_history.count, len(payment_history.payments))
+
+        for payment in payment_history.payments:
+            output_file.write(json.dumps(payment.to_dict()))
+            output_file.write('\n')
+
+    def requires(self):
+        pass
+
+    def output(self):
+        url_with_filename = url_path_join(self.output_root, "paypal_{}.json".format(self.client_mode))
+        return get_target_from_url(url_with_filename)
+
+
+class IntervalPullFromPaypalTask(PullFromPaypalTaskMixin, luigi.Task):
+    """Determines a set of dates to pull, and requires them."""
+
+    interval = luigi.DateIntervalParameter()
+    output_root = luigi.Parameter()
+
+    def requires(self):
+        args = {
+            'client_mode': self.client_mode,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'output_root': self.output_root,
+            'overwrite': self.overwrite,
+        }
+        for current_date in self.interval:
+            date_string = current_date.strftime('%Y-%m-%d')  # pylint: disable=no-member
+            partition_path_spec = HivePartition('dt', date_string).path_spec
+            url_with_filename = url_path_join(self.output_root, partition_path_spec)
+
+            yield SinglePullFromPaypalTask(
+                client_mode=self.client_mode,
+                client_id=self.client_id,
+                client_secret=self.client_secret,
+                output_root=url_with_filename,
+                run_date=current_date,
+            )
+
+    def output(self):
+        return [task.output() for task in self.requires()]
